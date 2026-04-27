cccdc6d63f0234d08d3d06d1c77001d4ca439c13
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-04-27 15:06:50 +0300

    feat: Changes for support Gedaq 10.0.0.0

--- Src/Gedaq.MySqlConnector/Attributes/ParametrAttribute.cs
+++ Src/Gedaq.MySqlConnector/Attributes/ParametrAttribute.cs
@@ -4,9 +4,28 @@ using System.Data;
 
 namespace Gedaq.MySqlConnector.Attributes
 {
+    /// <summary>
+    /// An attribute for specifying a parameter passed in a request.
+    /// </summary>
+    /// <seealso href="https://github.com/SoftStoneDevelop/Gedaq.MySqlConnector/blob/main/Documentation/Parametr.md"/>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
     public sealed class ParametrAttribute : Attribute
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parametrType">The type of parameter added to the generated query</param>
+        /// <param name="parametrName">Parameter name if it is a named parameter, there are also positional parameters <paramref name="position"/></param>
+        /// <param name="dbType"></param>
+        /// <param name="size"></param>
+        /// <param name="nullable"></param>
+        /// <param name="direction"></param>
+        /// <param name="sourceColumn"></param>
+        /// <param name="sourceColumnNullMapping"></param>
+        /// <param name="sourceVersion"></param>
+        /// <param name="scale"></param>
+        /// <param name="precision"></param>
+        /// <param name="methodParametrName">The name of the parameter in the generated method. If not specified, an abstract name will be used, such as parameter1, parameter2, etc.</param>
         public ParametrAttribute(
             Type parametrType,
             string parametrName,
@@ -19,7 +38,7 @@ namespace Gedaq.MySqlConnector.Attributes
             DataRowVersion sourceVersion = DataRowVersion.Current,
             byte scale = 0,
             byte precision = 0,
-            string methodParametrName = null)
+            string? methodParametrName = null)
         {
         }
     }
